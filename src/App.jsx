e2b483bad853dae329edd1570ed9995a3d985b86
prashant <prashantyadav9407@gmail.com>
2025-12-17 22:49:09 +0530

    font changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import SchoolERP from './pages/SchoolERP'; // Aapka Home Page
 import Contact from './pages/Contact';     // Aapka Naya Contact Page
+import './App.css';
 
 function App() {
   return (
